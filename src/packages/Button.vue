9452df2afd167bd9935fe15ef93b9784d0994197
李伟 <liwei@mininglamp.com>
2020-07-03 16:39:47 +0800

    Initial commit

---
+++ src/packages/Button.vue
@@ -0,0 +1,39 @@
+<template>
+	<button :class="btnClass">
+		<slot></slot>
+	</button>
+</template>
+
+<script lang="ts">
+import {Vue, Component, Prop } from 'vue-property-decorator';
+
+@Component({
+	name: "dui-button"
+})
+export default class Button extends Vue {
+	@Prop({type: String, default: "default"}) type!: string;
+	
+	get btnClass(){
+		return `btn-${this.type}`
+	}
+}
+</script>
+
+<style lang="less" scoped>
+.btn-default {
+	width: 120px;
+	height: 40px;
+	background-color: #ffaabb;
+	color: #fff;
+	outline: none;
+	border: none;
+}
+.btn-primary {
+	width: 120px;
+	height: 40px;
+	background-color: #00aaff;
+	color: #fff;
+	outline: none;
+	border: none;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+	  <Button type="primary">button</Button>
+  </div>
+</template>
+
+<script>
+import Button from '../packages/Button.vue';
+
+export default {
+  name: "Home",
+  components: {
+	  Button
+  }
+};
+</script>
